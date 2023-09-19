083b3b9ac25e4f232857bfffc7a6e9cf55789c76
ALEXA GUSHOFF <a.gushoff.23@abdn.ac.uk>
2023-09-19 08:43:22 +0000

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[]args){
+        //System.out.printf("%s%n%s%n", "Hello World", "world", "bla");
+       //System.out.print("Hello\n World\n");
+       String hello = "hello";
+       System.out.print(hello);
+    
+    }
+
+}
