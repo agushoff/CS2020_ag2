f3c4cdf941be8cb998681039db7852452dba9132
ALEXA GUSHOFF <a.gushoff.23@abdn.ac.uk>
2023-09-19 09:22:46 +0000

    Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,15 @@
+
+import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[]args){
         //System.out.printf("%s%n%s%n", "Hello World", "world", "bla");
        //System.out.print("Hello\n World\n");
        String hello = "hello";
        System.out.print(hello);
+       Scanner scan = new Scanner(System.in);
+       int wiederholen = scan.nextInt();
+       System.out.println(wiederholen);
+
     
     }
 
